830acc6dd2a95b2b0fdce0b2079d26fd223c61ac
ronalyssa <ronalyssa99@gmail.com>
2021-04-13 08:20:00 -0500

    started players controller and relation in model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,31 @@
 class PlayersController < ApplicationController
+    def index
+        players = Player.all
+    end
+
+    def show
+        player = Player.find(params[:id])
+    end
+
+    def new
+        player = Player.new
+        
+    end
+    
+    def create
+        player = Player.new(player_params)
+        if player.save 
+            redirect_to player_path(player)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :age, :vip_lounge_ids)
+        
+    end
+    
 end
